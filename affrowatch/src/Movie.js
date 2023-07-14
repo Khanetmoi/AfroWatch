96ee50736fe7af2e669a811eaf1e7eefe02afd11
Gedeon-King <122874949+Gedeon-King@users.noreply.github.com>
2023-07-14 14:19:45 +0000

    Update Movie.js
    added the post to the movie

--- affrowatch/src/Movie.js
+++ affrowatch/src/Movie.js
@@ -2,21 +2,19 @@ import React, { useState } from 'react';
 
 function MovieForm() {
   const [movieInfo, setMovieInfo] = useState({
-    movieFile: '',
-    trailer: '',
-    movieName: '',
-    description: '',
-    actors: '',
-    director: '',
-    ageRange: '',
-    videoLength: '',
-    genre: '',
-    status: '',
-    Image: '',
-    release_year: '',
-    age: '',
-    rating: '',
-    length: '',
+    movie_name: '',
+    movie_producer: '',
+    movie_actor: '',
+    movie_description: '',
+    movie_year_release: '',
+    movie_age_range: '',
+    movie_image: '',
+    movie_file: '',
+    movie_trailler_file: '',
+    movie_rating: '',
+    movie_genre: '',
+    movie_length: '',
+    movie_status: '',
   });
 
   const postData = async (url, data) => {
@@ -28,22 +26,20 @@ function MovieForm() {
         },
         body: JSON.stringify(data),
       });
-  
+
       if (!response.ok) {
         throw new Error('Error posting data');
       }
-  
+
       const responseData = await response.json();
       console.log('Response:', responseData);
       // Perform actions with the response data
-  
     } catch (error) {
       console.error('Error:', error);
     }
   };
 
-  const url = "https://myworklm.com/afrowatch/api/afrowatch_api_admin.php";
-  
+  const url = 'https://myworklm.com/afrowatch/api/afrowatch_api_movie.php';
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -55,10 +51,21 @@ function MovieForm() {
     // Perform actions with movieInfo, e.g., submit to server, etc.
     console.log(movieInfo);
     postData(url, {
-      movieFile: 'movie.mp4',
-      trailer: 'trailer.mp4',
-      movieName: 'Example Movie',
-      })
+      admin_id:"0",
+      movie_name: movieInfo.movie_name,
+      movie_producer: movieInfo.movie_producer,
+      movie_actor: movieInfo.movie_actor,
+      movie_description: movieInfo.movie_description,
+      movie_year_release: movieInfo.movie_year_release,
+      movie_age_range: movieInfo.movie_age_range,
+      movie_image: movieInfo.movie_image,
+      movie_file: movieInfo.movie_file,
+      movie_trailler_file: movieInfo.movie_trailler_file,
+      movie_rating: movieInfo.movie_rating,
+      movie_genre: movieInfo.movie_genre,
+      movie_length:"hhhhhhh",
+      movie_status: movieInfo.movie_status,
+    });
   };
 
   return (
@@ -66,131 +73,136 @@ function MovieForm() {
       <h2>Movie Information Form</h2>
       <form onSubmit={handleSubmit}>
         <label>
-          Movie File:
+          1 Movie File:
+          <input type="file" name="movie_file" value={movieInfo.movie_file} onChange={handleChange} required />
+        </label>
+        <br />
+        <label>
+          2 Trailer File:
           <input
             type="file"
-            name="movieFile"
+            name="movie_trailler_file"
+            value={movieInfo.movie_trailler_file}
             onChange={handleChange}
             required
           />
         </label>
         <br />
         <label>
-          Trailer:
+          3 Image File:
           <input
             type="file"
-            name="trailer"
-            value={movieInfo.trailer}
+            name="movie_image"
+            value={movieInfo.movie_image}
             onChange={handleChange}
             required
           />
         </label>
         <br />
         <label>
-          Image:
-          <input
-            type="file"
-            name="trailer"
-            value={movieInfo.Image}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <br />
-        <label>
-          Status:
+          4 Status:
           <input
             type="text"
-            name="movieName"
-            value={movieInfo.movieName}
+            name="movie_status"
+            value={movieInfo.movie_status}
             onChange={handleChange}
             required
           />
         </label>
-
         <br />
         <label>
-          Movie Name:
+          5 Movie Name:
           <input
             type="text"
-            name="movieName"
-            value={movieInfo.movieName}
+            name="movie_name"
+            value={movieInfo.movie_name}
             onChange={handleChange}
             required
           />
         </label>
         <br />
         <label>
-          Description:
+          6 Description:
           <textarea
-            name="description"
-            value={movieInfo.description}
+            name="movie_description"
+            value={movieInfo.movie_description}
             onChange={handleChange}
             required
           />
         </label>
         <br />
         <label>
-          Actors:
+          7 Actors:
           <input
             type="text"
-            name="actors"
-            value={movieInfo.actors}
+            name="movie_actor"
+            value={movieInfo.movie_actor}
             onChange={handleChange}
             required
           />
         </label>
         <br />
         <label>
-          Director:
+          8 Director:
           <input
             type="text"
-            name="director"
-            value={movieInfo.director}
+            name="movie_producer"
+            value={movieInfo.movie_producer}
             onChange={handleChange}
             required
           />
         </label>
         <br />
         <label>
-          Age Range:
+          9 Age Range:
           <input
             type="text"
-            name="ageRange"
-            value={movieInfo.ageRange}
+            name="movie_age_range"
+            value={movieInfo.movie_age_range}
             onChange={handleChange}
             required
           />
         </label>
         <br />
         <label>
-          Video Length:
+          10 Video Length:
           <input
             type="text"
-            name="videoLength"
-            value={movieInfo.videoLength}
+            name="movie_length"
+            value={movieInfo.movie_length}
             onChange={handleChange}
             required
           />
         </label>
         <br />
         <label>
-          Genre:
+          11 Genre:
           <input
             type="text"
-            name="genre"
-            value={movieInfo.genre}
+            name="movie_genre"
+            value={movieInfo.movie_genre}
             onChange={handleChange}
             required
           />
         </label>
         <br />
         <label>
-          Status:
+          12 Release Year:
           <input
             type="text"
-            name="status"
-            value={movieInfo.status}
+            name="movie_year_release"
+            value={movieInfo.movie_year_release}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          13 Rating:
+          <input
+            type="text"
+            name="movie_rating"
+            value={movieInfo.movie_rating}
             onChange={handleChange}
             required
           />
